fe75d334c2a0b33588e68906d41af309d7c8645e
ryandu3000 <117936875+ryandu3000@users.noreply.github.com>
2022-11-11 13:05:07 -0500

    Update Rectange.java

--- Rectange.java
+++ Rectange.java
@@ -1,3 +1,17 @@
 public class Rectangle()
 {
+  private int length;
+  private int width;
+  
+  public Rectangle(int l, int w)
+  {
+    length = l;
+    width = w;
+  }
+  
+  public int getArea()
+  {
+    return length * width;
+  }
+  
 }
